8ec06f4e1dad71c3eaea42bbdb126ab9d5b12f2f
redaalch <s133260695@taalim.ma>
2025-10-05 00:17:41 +0100

    fix: correct Navbar component layout and improve Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,12 +5,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["forest","dark","coffee"],
+    themes: ["forest", "dark", "coffee"],
   },
 };
