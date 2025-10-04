bf3d9b5581099acf5a3272680099d6dd36790a82
redaalch <s133260695@taalim.ma>
2025-10-05 00:10:37 +0100

    feat: add Navbar component and integrate routing with Tailwind CSS and DaisyUI
    - Added Navbar component for the header.
    - Integrated React Router for navigation between HomePage, CreatePage, and NoteDetailPage.
    - Configured Tailwind CSS with PostCSS and added DaisyUI for UI components.
    - Updated package.json to include necessary dependencies (axios, lucide-react, autoprefixer, daisyui, postcss, tailwindcss).
    - Created index.css to include Tailwind's base, components, and utilities.

--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Navbar/>
+    </div>
   )
 }
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router";
+import HomePage from "../pages/HomePage.jsx";
+import CreatePage from "../pages/CreatePage.jsx";
+import NoteDetailPage from "../pages/NoteDetailPage";
 
 function App() {
   return (
-    <div>
-        
+      <div data-theme="coffee">
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
