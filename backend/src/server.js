91569135828f83b2c868a20091c1d7f02670d056
redaalch <s133260695@taalim.ma>
2025-10-05 19:03:18 +0100

    feat: add static file serving and catch-all route for frontend in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,11 @@ import path from "path";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDb } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 const app = express();
 
 app.use(
@@ -27,6 +29,13 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res)=> {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+
 connectDb().then(() => {
   app.listen(5001, () => {
     console.log("server started on port :", PORT);
