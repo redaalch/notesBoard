d195d27161ea5870f9d0540ee82c29210f1a896f
redaalch <s133260695@taalim.ma>
2025-10-05 01:02:07 +0100

    feat: add CORS support, implement rate limiting, and create RateLimitedUI component

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,20 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
+
 app.use(rateLimiter);
 
 // app.use((req,res,next)=> {
--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../Components/Navbar";
+import RateLimitedUI from "../Components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function HomePage() {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("error fetching" + error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load Notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
-    <div className='min-h-screen'>
-      <Navbar/>
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">
+            Loading notes ...
+          </div>
+        )}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 mid:grid-cols2 lg:grid-cols-3 gap-6">
+            {notes.title} | {notes.content}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
