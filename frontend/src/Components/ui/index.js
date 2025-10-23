45e07bcb59f95e39eec3b2d3d7bf8301da520e98
redaalch <s133260695@taalim.ma>
2025-10-23 15:02:01 +0100

    feat: create index file for UI components to streamline exports

---
+++ frontend/src/Components/ui/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button.jsx";
+export { default as Surface } from "./Surface.jsx";
+export { default as Stack } from "./Stack.jsx";
+export { default as Card } from "./Card.jsx";
+export { default as Chip } from "./Chip.jsx";
+export { default as MetricTile } from "./MetricTile.jsx";
+export { default as Tag } from "./Tag.jsx";
